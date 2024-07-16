7753c4e7489d914ff66897e016d037a28e42e546
Neelesh <48306376+neeleshdubey567@users.noreply.github.com>
2024-07-16 20:23:43 +0530

    second commit

--- src/main/java/com/lcwd/user/service/respositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lcwd.user.service.respositories;
-
-import com.lcwd.user.service.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,String> {
-
-
-}
